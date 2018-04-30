dbee00af0456b1cc2b5a83e3ac4946f3bba24b93
Marcel Fahle <m.fahle@gmail.com>
2018-04-30 13:08:58 +0200

    external file maybe?

--- src/components/BackgroundVideoPlayer.js
+++ src/components/BackgroundVideoPlayer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Link from 'gatsby-link'
 import videojs from 'video.js'
+import './BackgroundVideoPlayer.css'
 import 'videojs-playlist'
 
 import Header from './Header'
