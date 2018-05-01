7c9321eafbdfdecd6c80a491404027c68a24c53b
Marcel Fahle <m.fahle@gmail.com>
2018-05-01 15:53:33 +0200

    adds sorting

--- src/pages/oeuvre.js
+++ src/pages/oeuvre.js
@@ -15,34 +15,35 @@ export default class Oeuvre extends React.Component {
     const { data } = this.props
     const items = data.allDatoCmsPagePortfolio.edges
     let offset = 0
-    const isSlower = getRandomInt(0, 1) ? true : false
 
     // e.node.pictures[0].resolutions.aspectRatio > 1 ? i * 0.6 : i * 1
 
     return (
       <PageWrapper>
         <Header size="small" siteTitle="THE FEELING" />
+
         <Parallax ref={ref => (this.parallax = ref)} pages={items.length * 0.7}>
-          {items.map((e, i) => {
-            return (
-              <Parallax.Layer
-                key={i}
-                offset={i * 0.5}
-                speed={e.node.speed / 30}
-                factor={e.node.scrollPageHeight / 100 || 0.7}
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  width: '100%',
-                  borderBottom: '1px solid red',
-                  borderTop: '1px solid blue',
-                }}
-              >
-                <PortfolioItem data={e.node} />
-              </Parallax.Layer>
-            )
-          })}
+          {items &&
+            items.map((e, i) => {
+              return (
+                <Parallax.Layer
+                  key={i}
+                  offset={i * 0.5}
+                  speed={e.node.speed / 30 || 1}
+                  factor={e.node.scrollPageHeight / 100 || 0.7}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: '100%',
+                    borderBottom: '1px solid red',
+                    borderTop: '1px solid blue',
+                  }}
+                >
+                  <PortfolioItem data={e.node} />
+                </Parallax.Layer>
+              )
+            })}
         </Parallax>
       </PageWrapper>
     )
@@ -51,7 +52,7 @@ export default class Oeuvre extends React.Component {
 
 export const query = graphql`
   query PortfolioQuery {
-    allDatoCmsPagePortfolio {
+    allDatoCmsPagePortfolio(sort: { fields: [position] }) {
       edges {
         node {
           title
