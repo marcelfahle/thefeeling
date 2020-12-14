097360020c8293ae6f983b6b79b79e132f31fe3d
Marcel Fahle <m.fahle@gmail.com>
2020-12-14 21:43:59 +0100

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,23 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  siteMetadata: {
-    title: 'THE FEELING',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-layout',
-    {
-      resolve: 'gatsby-source-datocms',
-      options: {
-        apiToken: process.env.DATO_API,
-        preview: false,
-        disableLiveReload: false,
-      },
-    },
-  ],
+	siteMetadata: {
+		title: 'THE FEELING',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-transformer-remark',
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-layout',
+		{
+			resolve: 'gatsby-source-datocms',
+			options: {
+				apiToken: process.env.DATO_API,
+				preview: false,
+				disableLiveReload: false,
+			},
+		},
+	],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import styled, { createGlobalStyle } from 'styled-components'
-import { graphql, StaticQuery } from 'gatsby'
+import styled, {createGlobalStyle} from 'styled-components'
+import {graphql, StaticQuery} from 'gatsby'
+
+//import 'react-modal-video/scss/modal-video.scss';
 
 const GlobalStyle = createGlobalStyle`
 	html, body { 
@@ -25,42 +27,42 @@ const Wrapper = styled.div`
 `
 
 const colors = [
-  {
-    leftBg: '#e53212',
-    leftText: '#2b632d',
-    rightBg: '#c78800',
-    rightText: '#ffed00',
-  },
-  {
-    leftBg: '#009ee3',
-    leftText: '#273582',
-    rightBg: '#e53212',
-    rightText: '#c78800',
-  },
-  {
-    leftBg: '#009ee3',
-    leftText: '#ffffff',
-    rightBg: '#2b632d',
-    rightText: '#c78800',
-  },
-  {
-    leftBg: '#b51614',
-    leftText: '#009ee3',
-    rightBg: '#273582',
-    rightText: '#e30513',
-  },
-  {
-    leftBg: '#ffed00',
-    leftText: '#273582',
-    rightBg: '#e30513',
-    rightText: '#b51614',
-  },
+	{
+		leftBg: '#e53212',
+		leftText: '#2b632d',
+		rightBg: '#c78800',
+		rightText: '#ffed00',
+	},
+	{
+		leftBg: '#009ee3',
+		leftText: '#273582',
+		rightBg: '#e53212',
+		rightText: '#c78800',
+	},
+	{
+		leftBg: '#009ee3',
+		leftText: '#ffffff',
+		rightBg: '#2b632d',
+		rightText: '#c78800',
+	},
+	{
+		leftBg: '#b51614',
+		leftText: '#009ee3',
+		rightBg: '#273582',
+		rightText: '#e30513',
+	},
+	{
+		leftBg: '#ffed00',
+		leftText: '#273582',
+		rightBg: '#e30513',
+		rightText: '#b51614',
+	},
 ]
 const color = colors[Math.floor(Math.random() * colors.length)]
 
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
+export default ({children}) => (
+	<StaticQuery
+		query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -86,26 +88,26 @@ export default ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <Wrapper>
-        <GlobalStyle />
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: '' },
-            { name: 'keywords', content: '' },
-            { name: 'viewport', content: 'width=device-width, minimal-ui' },
-          ]}
-        />
-        <div>
-          {/*children(Object.assign({}, { data }, { bg: data.bg, color: color }))*/}
-          {React.Children.map(children, child =>
-            React.cloneElement(child, { bg: data.bg, color: color })
-          )}
-        </div>
-      </Wrapper>
-    )}
-  />
+		render={data => (
+			<Wrapper>
+				<GlobalStyle />
+				<Helmet
+					title={data.site.siteMetadata.title}
+					meta={[
+						{name: 'description', content: ''},
+						{name: 'keywords', content: ''},
+						{name: 'viewport', content: 'width=device-width, minimal-ui'},
+					]}
+				/>
+				<div>
+					{/*children(Object.assign({}, { data }, { bg: data.bg, color: color }))*/}
+					{React.Children.map(children, child =>
+						React.cloneElement(child, {bg: data.bg, color: color})
+					)}
+				</div>
+			</Wrapper>
+		)}
+	/>
 )
 
 //Layout.propTypes = {
