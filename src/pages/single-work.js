a6e7bff8051d6cbcc84149bddc9e66109309fc52
Marcel Fahle <m.fahle@gmail.com>
2020-12-31 23:47:17 +0100

    play button

--- src/pages/single-work.js
+++ src/pages/single-work.js
@@ -56,7 +56,7 @@ const StartVideoButton = styled.div`
     z-index: 60;
   }
   @media (orientation: portrait) {
-    top: calc(calc(var(--vh, 1vh) * 50) - 12px);
+    top: calc(calc(var(--vh, 1vh) * 50) - 0px);
   }
 `
 
