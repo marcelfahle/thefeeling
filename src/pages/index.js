55f181417eb942e0d301c8667e4232601ceec29b
Marcel Fahle <m.fahle@gmail.com>
2022-10-13 06:11:42 +0200

    moves to bold

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { graphql } from 'gatsby'
 
 import BackgroundVideoPlayer from '../components/BackgroundVideoPlayer'
 
-function IndexPage({ data, color }) {
+function IndexPage({ color }) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://app.boldvideo.io/api/playlists/geg8b`, {
+      headers: {
+        Authorization: process.env.BOLD_API,
+      },
+    })
+    const transform = (data) =>
+      data.videos.map((v) => ({
+        videoUrl: `https://stream.mux.com/${v.playback_id}/medium.mp4`,
+      }))
+
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://app.boldvideo.io/api/playlists/geg8b`,
+          {
+            headers: {
+              Authorization: process.env.BOLD_API,
+            },
+          }
+        )
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          )
+        }
+        let actualData = await response.json()
+        setData(transform(actualData))
+        setError(null)
+      } catch (err) {
+        setError(err.message)
+        setData(null)
+      } finally {
+        setLoading(false)
+      }
+    }
+    getData()
+  }, [])
+
+  console.log('data', data)
+
   return (
-    <div>
-      <BackgroundVideoPlayer
-        color={color}
-        playlist={data.datoCmsPageHome.videoPlaylist}
-      />
-    </div>
+    <div>{data && <BackgroundVideoPlayer color={color} playlist={data} />}</div>
   )
 }
 
 export default IndexPage
-
-export const query = graphql`
-  query IndexQuery {
-    datoCmsPageHome {
-      videoPlaylist {
-        videoUrl
-        logoColors
-      }
-    }
-  }
-`
